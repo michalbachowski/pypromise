adddeaa5b0bed84fd053f87277fa65e4b7e8858c
Michał - MiB - Bachowski <mib@heroes.net.pl>
2013-02-05 14:13:22 +0100

    Added promise.CallbackList implementation with test

--- src/promise.py
+++ src/promise.py
@@ -3,11 +3,83 @@
 
 
 class CallbackList(object):
-    pass
+    """
+    Simple list of callback that gets fired on demand.
+    If callback will be attached after resolvement will fire immediately
+    """
+
+    def __init__(self):
+        """
+        Object initialization
+        """
+        self._callbacks = []
+        self._args = ()
+        self._kwargs = {}
+        self._resolved = False
+
+    def _fire(self, callbacks):
+        """
+        Fires given callbacks
+        """
+        for c in callbacks:
+            c(*self._args, **self._kwargs)
+
+    def done(self, *args):
+        """
+        Attaches given callback (or callbacks)
+        """
+        if self.cancelled:
+            return self
+        if self._resolved:
+            self._fire(args)
+        else:
+            self._callbacks.extend(args)
+        return self
+
+    def resolve(self, *args, **kwargs):
+        """
+        Resolves callback with given attributes
+        """
+        if self.cancelled:
+            return self
+        self._resolved = True
+        self._args = args
+        self._kwargs = kwargs
+        self._fire(self._callbacks)
+        return self
+
+    def cancel(self):
+        """
+        Terminates resolution
+        """
+        self._callbacks = None
+        return self;
+
+    @property
+    def cancelled(self):
+        """
+        Checkes whether resolution was cancelled
+        """
+        return self._callbacks is None
+
+    @property
+    def resolved(self):
+        """
+        Returns resolution status
+        """
+        return self._resolved
 
 
 class Deferred(object):
-    pass
+    """
+    Deferred object
+    """
+
+    def then(self, success, error):
+        """
+        Convenient wrapper for self.done() and self.fail() subsequent calls
+        """
+        return self
 
 
 def when(*args):
--- tests/callback_list_test.py
+++ tests/callback_list_test.py
@@ -20,12 +20,139 @@ from promise import CallbackList
 class CallbackListTestCase(unittest.TestCase):
 
     def setUp(self):
-        logging.basicConfig()
-        self.log = logging.getLogger()
         self.mox = mox.Mox()
 
     def tearDown(self):
         self.mox.UnsetStubs()
+
+    def test_done_expects_no_arguments(self):
+        err = False
+        try:
+            CallbackList().done()
+        except TypeError:
+            err = True
+        self.assertFalse(err)
+
+    def test_done_allows_to_pass_variable_number_of_input_args(self):
+        err = False
+        try:
+            CallbackList().done()
+            CallbackList().done(1)
+            CallbackList().done(1, 2)
+            CallbackList().done(1, 2, 3)
+            CallbackList().done(1, 2, 3, 4)
+        except TypeError:
+            err = True
+        self.assertFalse(err)
+
+    def test_done_returns_self_so_chaining_is_possible(self):
+        err = False
+        try:
+            CallbackList().done().done()
+        except AttributeError:
+            err = True
+        self.assertFalse(err)
+
+    def test_resolve_expects_no_arguments(self):
+        err = False
+        try:
+            CallbackList().resolve()
+        except TypeError:
+            err = True
+        self.assertFalse(err)
+
+    def test_resolve_allows_to_pass_variable_number_of_input_args(self):
+        err = False
+        try:
+            CallbackList().resolve()
+            CallbackList().resolve(1)
+            CallbackList().resolve(1, 2)
+            CallbackList().resolve(1, 2, 3)
+            CallbackList().resolve(1, 2, 3, 4)
+        except TypeError:
+            err = True
+        self.assertFalse(err)
+
+    def test_resolve_returns_self_so_chaining_is_possible(self):
+        err = False
+        try:
+            CallbackList().resolve().resolve()
+        except AttributeError:
+            err = True
+        self.assertFalse(err)
+
+    def test_resolve_fires_callbacks(self):
+        # prepare
+        c = self.mox.CreateMockAnything()
+        c()
+        self.mox.ReplayAll()
+
+        # test
+        CallbackList().done(c).resolve()
+
+        # verify
+        self.mox.VerifyAll()
+
+    def test_done_after_resolvement_fires_callbacks_immediately(self):
+        # prepare
+        c = self.mox.CreateMockAnything()
+        c()
+        self.mox.ReplayAll()
+
+        # test
+        CallbackList().resolve().done(c)
+
+        # verify
+        self.mox.VerifyAll()
+
+    def test_all_given_callbacks_are_called(self):
+        # pre-resolve callbacks
+        pre = []
+        post = []
+        for i in xrange(0, 10):
+            t = self.mox.CreateMockAnything()
+            t()
+            pre.append(t)
+        for i in xrange(0, 10):
+            t = self.mox.CreateMockAnything()
+            t()
+            post.append(t)
+        self.mox.ReplayAll()
+
+        # test
+        CallbackList().done(*pre).resolve().done(*post)
+
+        # verify
+        self.mox.VerifyAll()
+
+    def test_resolved_returns_resolution_status_1(self):
+        self.assertFalse(CallbackList().resolved)
+    
+    def test_resolved_returns_resolution_status_2(self):
+        self.assertFalse(CallbackList().done().resolved)
+    
+    def test_resolved_returns_resolution_status_3(self):
+        self.assertTrue(CallbackList().resolve().resolved)
+
+    def test_cancel_terminates_resolution(self):
+        c = self.mox.CreateMockAnything()
+        c()
+        self.mox.ReplayAll()
+
+        CallbackList().done(c).cancel().resolve()
+        CallbackList().cancel().done(c).resolve()
+        CallbackList().resolve().cancel().done(c)
+
+        self.assertRaises(mox.ExpectedMethodCallsError, self.mox.VerifyAll)
+
+    def test_cancelled_returns_cancellation_status_1(self):
+        self.assertFalse(CallbackList().cancelled)
+    
+    def test_cancelled_returns_cancellation_status_2(self):
+        self.assertTrue(CallbackList().cancel().cancelled)
+    
+    def test_cancelled_returns_cancellation_status_3(self):
+        self.assertTrue(CallbackList().cancel().resolve().cancelled)
 
 
 if "__main__" == __name__:
