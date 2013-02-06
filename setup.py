949054ca05ee7e7b90189ccb79837482cbe247f2
Michał - MiB - Bachowski <mib@heroes.net.pl>
2013-02-06 11:48:48 +0100

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='PyPromise',\
+    version='1.0',\
+    description='Python Promise implementation',\
+    license='New BSD License',\
+    author='Michał Bachowski',\
+    author_email='michal@bachowski.pl',\
+    package_dir={'': 'src'},\
+    py_modules=['promise'])
