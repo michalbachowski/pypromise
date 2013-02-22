fe4663cd61b7fa52bfaf530a194f06f3a8edaa1e
Michał Bachowski <mib@heroes.net.pl>
2013-02-22 10:05:26 +0100

    Refactored promise_test to work with 'mock' mocking library

--- tests/promise_test.py
+++ tests/promise_test.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 
 # hack for loading modules
-import _path
-_path.fix()
+from _path import fix, mock
+fix()
 
 ##
 # python standard library
 #
+from functools import partial
 import unittest
-import mox
 
 ##
 # promise modules
@@ -20,18 +20,10 @@ from promise import Promise
 class PromiseTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.mox = mox.Mox()
-
-    def tearDown(self):
-        self.mox.UnsetStubs()
+        self.d = mock.Mock()
 
     def test_init_expects_one_arg_1(self):
-        err = False
-        try:
-            Promise()
-        except TypeError:
-            err = True
-        self.assertTrue(err)
+        self.assertRaises(TypeError, Promise)
     
     def test_init_expects_one_arg_2(self):
         err = False
@@ -42,67 +34,23 @@ class PromiseTestCase(unittest.TestCase):
         self.assertFalse(err)
     
     def test_promise_works_as_proxy_for_given_object(self):
-        # prepare
-        d = self.mox.CreateMockAnything()
-        d.foo()
-        self.mox.ReplayAll()
-
-        # test
-        Promise(d).foo()
-
-        # verify
-        self.mox.VerifyAll()
+        Promise(self.d).foo()
+        self.d.foo.assert_called_once_with()
     
     def test_promise_rejects_call_to_resolve_method(self):
-        # prepare
-        d = self.mox.CreateMockAnything()
-        d.resolve()
-        self.mox.ReplayAll()
-
-        # test
-        err = False
-        try:
-            Promise(d).resolve()
-        except RuntimeError:
-            err = True
-
-        # verify
-        self.assertTrue(err)
-        self.assertRaises(mox.ExpectedMethodCallsError, self.mox.VerifyAll)
+        self.assertRaises(RuntimeError, partial(getattr, Promise(self.d), 
+                'resolve'))
+        self.assertEqual(self.d.resolve.call_count, 0)
     
     def test_promise_rejects_call_to_reject_method(self):
-        # prepare
-        d = self.mox.CreateMockAnything()
-        d.resolve()
-        self.mox.ReplayAll()
-
-        # test
-        err = False
-        try:
-            Promise(d).reject()
-        except RuntimeError:
-            err = True
-
-        # verify
-        self.assertTrue(err)
-        self.assertRaises(mox.ExpectedMethodCallsError, self.mox.VerifyAll)
+        self.assertRaises(RuntimeError, partial(getattr, Promise(self.d), 
+                'reject'))
+        self.assertEqual(self.d.reject.call_count, 0)
     
     def test_promise_rejects_call_to_cancel_method(self):
-        # prepare
-        d = self.mox.CreateMockAnything()
-        d.resolve()
-        self.mox.ReplayAll()
-
-        # test
-        err = False
-        try:
-            Promise(d).cancel()
-        except RuntimeError:
-            err = True
-
-        # verify
-        self.assertTrue(err)
-        self.assertRaises(mox.ExpectedMethodCallsError, self.mox.VerifyAll)
+        self.assertRaises(RuntimeError, partial(getattr, Promise(self.d), 
+                'cancel'))
+        self.assertEqual(self.d.cancel.call_count, 0)
 
 
 if "__main__" == __name__:
